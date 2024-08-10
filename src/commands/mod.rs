9ff4142aa34cbe1aaf039ac9638f4bb4cc7250f8
j1nxie <rylieeeeexd@gmail.com>
2024-08-10 13:02:09 +0700

    chore: cleanup

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,13 +1,7 @@
-use poise::serenity_prelude::UserId;
-
 use crate::Context;
 
 pub mod help;
 pub mod status;
-
-pub(crate) fn get_bot_id(ctx: Context<'_>) -> UserId {
-    ctx.cache().current_user().id
-}
 
 pub(crate) fn get_bot_avatar(ctx: Context<'_>) -> String {
     ctx.cache().current_user().avatar_url().unwrap_or_default()
