14845ecdfaedc643628b393c469c7eceec3c7835
j1nxie <rylieeeeexd@gmail.com>
2025-03-20 17:32:17 +0700

    fix: forgot to cascade on delete

--- migrations/20250320101930_subscriptions.up.sql
+++ migrations/20250320101930_subscriptions.up.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS "manga_subscriptions" (
     id INTEGER PRIMARY KEY,
     manga_dex_id TEXT NOT NULL,
     user_id TEXT NOT NULL,
-    FOREIGN KEY (manga_dex_id) REFERENCES manga (manga_dex_id)
+    FOREIGN KEY (manga_dex_id) REFERENCES manga (manga_dex_id) ON DELETE CASCADE
 );
