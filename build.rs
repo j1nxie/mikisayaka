1bb1800b945076372306339216a5565601701313
j1nxie <rylieeeeexd@gmail.com>
2025-07-25 10:24:42 +0700

    build: rerun build if migrations change

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ use anyhow::Error;
 use vergen_gitcl::{Emitter, GitclBuilder};
 
 pub fn main() -> Result<(), Error> {
+    println!("cargo:rerun-if-changed=migrations");
     if let Ok(sha) = std::env::var("VERGEN_GIT_SHA") {
         if sha != "unknown" {
             println!("cargo:rustc-env=VERGEN_GIT_SHA={sha}");
