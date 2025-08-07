33e738ce0bd42cf3ce9fada19fe763dd208c9749
j1nxie <rylieeeeexd@gmail.com>
2025-08-07 15:42:05 +0700

    release: bump version to 2025.8.0

--- src/constants/version.rs
+++ src/constants/version.rs
@@ -24,7 +24,7 @@ pub fn get_version() -> String {
     if let Ok(semver) = semver {
         format!(
             "v{} - {} [[`{2}`](https://github.com/j1nxie/mikisayaka/commit/{2})]",
-            semver, MADOKA_MAGICA[6], git_sha,
+            semver, MADOKA_MAGICA[7], git_sha,
         )
     } else {
         tracing::warn!("couldn't parse a semver out of Cargo.toml? defaulting to 0.0.0-unknown.");
