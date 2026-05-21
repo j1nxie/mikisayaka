d6a6db9da17433942ce27662b9cf6566aa755d53
j1nxie <rylieeeeexd@gmail.com>
2026-05-21 15:14:29 +0700

    chore: update poise version string

--- src/constants.rs
+++ src/constants.rs
@@ -7,6 +7,6 @@ pub mod music;
 pub mod version;
 pub mod zenless;
 
-pub static POISE_VERSION: &str = "0.6.1";
+pub static POISE_VERSION: &str = "0.6.2";
 pub static STARTUP_TIME: LazyLock<std::time::SystemTime> =
     LazyLock::new(std::time::SystemTime::now);
